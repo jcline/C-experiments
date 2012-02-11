c891792210e605cc87675f8a44df567f238eccb0
James Cline <james.cline@gatech.edu>
2012-02-10 21:24:24 -0500

    untested oop example

---
+++ obj_test.c
@@ -0,0 +1,64 @@
+
+// constructor
+int obj_test_Ctor(struct obj_test_t* obj, const char* name, size_t len, int val)
+{
+	obj = (struct obj_test_t*) malloc(sizeof(struct obj_test_t));
+	if(obj == NULL)
+		return 1;
+
+	obj->name = (char*) malloc(sizeof(char)*len);
+	if(obj->name == NULL)
+	{
+		free(obj);
+		return 2;
+	}
+
+	obj->name = strncpy(obj->name, name, len);
+
+	obj->len = len;
+	obj->val = val;
+
+	return 0;
+}
+
+// destructor
+void obj_test_Dtor(struct obj_test_t* obj)
+{
+	free(obj->name);
+	free(obj);
+}
+
+// getters
+char* obj_test_get_name(struct obj_test_t* obj)
+{
+	if(obj)
+		return obj->name;
+	return NULL;
+}
+
+int obj_test_get_val(struct obj_test_t* obj)
+{
+	if(obj)
+		return obj->val;
+	return 0;
+}
+
+// setters
+void obj_test_set_name(struct obj_test_t* obj, const char* name, size_t len)
+{
+	if(obj)
+	{
+		if(len > obj->len)
+		{
+			obj->name = (char*) realloc(sizeof(char)*len);
+		}
+
+		obj->name = strncpy(obj->name, name, len);
+	}
+}
+
+void obj_test_set_val(struct obj_test_t* obj, int val)
+{
+	if(obj)
+		obj->val = val;
+}
---
+++ obj_test.h
@@ -0,0 +1,20 @@
+struct obj_test_t
+{
+	int val;
+	char* name;
+	size_t len;
+};
+
+// constructor
+int obj_test_Ctor(struct obj_test_t* obj, const char* name, size_t len, int val);
+
+// destructor
+void obj_test_Dtor(struct obj_test_t* obj);
+
+// getters
+char* obj_test_get_name(struct obj_test_t* obj);
+int obj_test_get_val(struct obj_test_t* obj);
+
+// setters
+void obj_test_set_name(struct obj_test_t* obj, const char* name, size_t len);
+void obj_test_set_val(struct obj_test_t* obj, int val);
