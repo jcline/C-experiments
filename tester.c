0a228c0c0939530f94e55982169438a4871f5d08
James Cline <james.cline@gatech.edu>
2012-02-10 21:57:57 -0500

    rough draft of tester

--- tester.c
+++ tester.c
@@ -1,10 +1,27 @@
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 
+#include "obj_test.h"
 
+int obj_tester()
+{
+	struct obj_test_t* obj;
+
+	char* name = "Object";
+
+	obj_test_Ctor(obj, name, strlen(name), 9001);
+
+	assert(obj);
+	assert(obj->val == 9001);
+	assert(obj->len == strlen(name));
+}
 
 
 int main()
 {
+
+	obj_tester();
+
 	return 0;
 }
